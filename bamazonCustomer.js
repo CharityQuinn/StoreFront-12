2b88d63d51461a0a6b4c0f0f433eef6e8d57b46f
CharityQuinn <charityaquinn@gmail.com>
2019-01-27 19:25:14 -0500

    Beginings of homework 12, set up db products, begining js page

---
+++ bamazonCustomer.js
@@ -0,0 +1,87 @@
+// Require sql
+var mysql = require("mysql");
+
+// Require Inquirer
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Establish port 3306
+  port: 3306,
+
+  // Username
+  user: "root",
+
+  // Password
+  password: "Root",
+  database: "bamazon"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  askUser();
+});
+
+// The first should ask them the ID of the product they would like to buy.
+//* The second message should ask how many units of the product they would like to buy.
+function askUser() {
+  inquirer
+  .prompt({
+    name: "action",
+    type: "list",
+    message: "What would you like to buy from our Store?",
+    choices: [
+      "Mops",
+      "Shoes",
+      "Socks",
+      "Umbrella",
+      "Hats",
+      "Water Bottles",
+      "Lunch-Box",
+      "Televisions",
+      "Laundry-Basket",
+      "Vitamins",
+      "exit"
+
+    ]
+  })
+  .then(function(answer) {
+    switch(answer.action) {
+      case "Products":
+      products();
+      break;
+
+      case "Departments":
+      departments();
+      break;
+
+      case "price":
+      price();
+      break;
+
+      case "quantity":
+      quantity();
+      break;
+    }
+  })
+
+}
+
+
+function products () {
+
+}
+
+function departments () {
+
+}
+
+function price () {
+
+}
+
+function quantity () {
+
+}
---
+++ index.sql
@@ -0,0 +1,42 @@
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+
+CREATE TABLE products(
+  PRIMARY KEY (item_id),
+  product_name VARCHAR(40),
+  department_name VARCHAR(40),
+  price INTEGER(10),
+  stock_quantity INTEGER(20);
+);
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Aunt Mabel Mops', "Cleaning Supplies", 5.00, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity)  
+VALUES ('Dr. Martens shoes', "Footware",100, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Wannamaker hats', 'Headware', 25, 40);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Bezos Umbrellas', 'Outerware', 15, 50);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Dr. Martens Socks', 'Footware', 11, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Lifefactory Water Bottles', 'Housewares', 13, 39);
+INSERT INTO products (product_name, department_name, price, stock_quantity)  
+VALUES ('Lifefactory', 'Lunch Boxes', 'Housewares', 12, 70);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('TSL Televisions', 'AudioVisual', 300, 15);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Lifefactory Laundry Basket', 'Housewares', 8, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity) 
+VALUES ('Unicare Vitamins', 'Groceries', 15, 200);
+
+
+SELECT * FROM products;
+
+-- show ALL books with products
+-- INNER JOIN will only return all matching VALUES from both tables
+SELECT products, department_name, price, stock_quantity
+FROM products
+
