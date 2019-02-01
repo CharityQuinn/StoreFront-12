3e9d975d86b29f2f2dabe2eedbebb057dbad66b2
CharityQuinn <charityaquinn@gmail.com>
2019-02-01 18:57:35 -0500

    A new, simpler way of doing the work on this homework

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,48 +14,22 @@ var connection = mysql.createConnection({
   database: "bamazon"
 });
 
+var userProdName;
+
 connection.connect(function (err) {
   if (err) throw err;
-  console.log("connected as id " + connection.threadId + "\n");
-
+  console.log("connected as id " + connection.threadId);
 });
 
-
-// Establish an array to hold the incoming results from the db
-let prodArr = [];
+var prodArr = [];
 const queryString = connection.query("SELECT * FROM products", function (err, res) {
-  if (err) throw (err);
   prodArr = res;
-  console.log("Available products\n");
-  for (var x = 0; x < res.length; x++) {
-    console.log([
-      res[x].item_id, res[x].product_name, res[x].price
-    ]);
+  console.table((prodArr));
 
-  }
-
-  startFunction(prodArr);
+  askUser();
+ 
 });
 
-function startFunction() {
-  inquirer
-    .prompt({
-      name: "questionOne",
-      type: "list",
-      message: "Do you want to buy something?",
-      choices: ["Yes", "No"]
-    })
-    .then(function (answer) {
-      // based on their answer, either call the bid or the post functions
-      if (answer.questionOne === "Yes") {
-        console.log("User picked Yes, moving to askUser");
-        askUser(prodArr);
-      } else if (answer.questionOne === "No") {
-        console.log("Okay Bye")
-        connection.end();
-      }
-    });
-}
 
 
 // The first should ask them the ID of the product they would like to buy.
@@ -63,86 +37,56 @@ function startFunction() {
 function askUser() {
   inquirer
     .prompt([{
-        name: "input1",
-        type: "rawlist",
-        message: "Please enter the number at the left of the product you would like."
+        name: "action",
+        type: "list",
+        message: "What would you like to buy from our Store?",
+        choices: ['Aunt Mabels Mops', 'Dr. Martens shoes', 'Wannamaker hats', 'Bezos Umbrellas', 'Dr. Martens Socks', 'Lifefactory Water Bottles', 'Lifefactory Lunch Boxes', 'TSL Televisions', 'Lifefactory Laundry Basket', 'Unicare Vitamins'],
+           
       },
       {
+        // Ask user how many of the product they would like to buy
         name: "howMany",
-        type: "input2",
+        type: "input",
         message: "How many of the units would you like to buy?",
-        validate: function (input2) {
-          if (input2 < 11 || input2 > 20) {
-            console.log("Please enter a number between 11 and 20!");
-           startFunction();
-          }
-          return !isNaN(input2);
-        },
+        validate: function (input) {
+          return !isNaN(input);
+        }
        
-        filter: function (input2) {
-          const userQuantity = parseInt(input2);
-          console.log(userQuantity);
-          return userQuantity;
-        }
       }
-    ])
-    .then(function (input1, userQuantity) {
-      // get the information of the chosen item
-      var chosenItem;
-      for (var i = 0; i < prodArr.length; i++) {
-        if (prodArr[i].item_id === input1) {
-          chosenItem = prodArr[i];
-          quantity(chosenItem, userQuantity);
+    ]) // process the user choices
+    .then(function(answer) {
+      // set up variable to hold the user choices
+      const item = answer.action;
+      const amount = parseInt(answer.howMany);
+      // query the db for the necessary info about the product the user chose 
+      const queryDB = "SELECT * FROM products WHERE product_name = ?";
+      connection.query(queryDB, item, function (err, results) {
+        // set up variables for the user's choice, the stock quantity and price of the product
+        const dbAmount = parseInt(results[0].stock_quantity);
+        const dbPrice = parseInt(results[0].price);
+        console.log("amount",amount);
+        console.log("item",item);
+        console.log("dbAmount",dbAmount);
+
+        if(err) throw err;
+        // Validate if there is enough product in stock to fulfill the user's order
+        if (amount <= dbAmount && amount > 0) {
+         // set variables for the new stock quantity and price for the user
+          const newDbAmount = dbAmount - amount;
+          const userPrice = dbPrice * amount;
+          // update the db with the new stock quantity
+          const updateDB = "UPDATE products SET stock_quantity = ? WHERE product_name = ?";
+          
+          connection.query(updateDB, [newDbAmount, item], function (err, results) {
+            // Let the user know we like them and they owe money
+            console.log(`Thank you for purchasing ${amount} ${item}.  You owe us around, (this number is rounded) $${userPrice}! Come again soon!`);
+            connection.end();
+          });
+        } else {
+          console.log("\r\n Sorry, we do not have that amount.  Please try again. \r\n");
+          askUser();
         }
-      }
+      })
     })
-}
 
-
-
-
-//const userId = answer.action;
-//console.log("The user picked " + userId);
-
-function products() {
-  for (var x = 0; x < prodArr.length; x++) {
-    console.log([
-      prodArr[x].item_id, prodArr[x].product_name, prodArr[x].price
-    ]);
-  };
-
-};
-
-
-
-function price() {
-  var goodSale = parseInt(prodArr.price) * parseInt(userQuantity);
-
-  console.log("Thank you for your purchase.  You owe us " + goodSale)
-  startFunction();
-};
-
-function quantity(userQuantity,chosenItem) {
-
-  for (var i = 0; i < prodArr.length; i++) {
-    if (prodArr[i].item_id === chosenItem) {
-      if (prodArr.stock_quantity <= userQuantity) {
-        // If a matching product is found, deplete stock and send to price
-        prodArr.stock_quantity = parseInt(prodArr.stock_quantity) - parseInt(userQuantity);
-        price(userQuantity, prodArr.price)
-        connection.query(
-          "UPDATE products SET ? WHERE ?",
-          [{
-              item_id: chosenItem
-            },
-            {
-              stock_quantity: stock_quantity
-            },
-            console.log("New Quantity placed successfully!")
-          ])
-
-      }
-    }
-
-  }
 }
